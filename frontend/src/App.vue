f0675344bd676c861b278391d746cd69cb6e177a
ryumaanai <ryuma.anai@di-v.co.jp>
2020-12-16 01:08:44 +0900

    Appからコンポーネントを読み込む

---
+++ frontend/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <hello></hello>
+</template>
+
+<script>
+import Hello from "./components/Hello.vue";
+
+export default {
+  components: {
+    Hello
+  }
+};
+</script>
+
+<style></style>
